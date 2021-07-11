dcfdf34ef263ccdf3653dfa59b60dbf1d9a38ee7
jeff <jeffhogg86@gmail.com>
2021-07-11 11:50:51 -0700

    readme, meta

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,13 @@
-import React, { ReactNode } from 'react';
-import { Container } from 'react-bootstrap';
-import Head from 'next/head';
-import Header from './Header';
-import Footer from './Footer';
+import React, { ReactNode } from 'react'
+import { Container } from 'react-bootstrap'
+import Head from 'next/head'
+import Header from './Header'
+import Footer from './Footer'
 
 type Props = {
-  children?: ReactNode;
-  title?: string;
-};
+  children?: ReactNode
+  title?: string
+}
 
 const Layout = ({ children, title = 'CardShop' }: Props) => (
   <div>
@@ -15,6 +15,14 @@ const Layout = ({ children, title = 'CardShop' }: Props) => (
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content="Buy the hottest sports cards!" />
+      <meta property="og:type" content="website" />
+      {/* <meta property="og:url" content="https://" /> */}
+      {/* <meta
+        property="og:image"
+        content="https://jeffehogg.com/images/homepage.png"
+      /> */}
       <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
@@ -26,6 +34,6 @@ const Layout = ({ children, title = 'CardShop' }: Props) => (
     </main>
     <Footer />
   </div>
-);
+)
 
-export default Layout;
+export default Layout
