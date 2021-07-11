79a1179c9e2340b07ade299112f57a96b3d8830b
jeff <jeffhogg86@gmail.com>
2021-07-11 13:33:32 -0700

    fix db connection

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -2,12 +2,9 @@ import dbConnect from '../../../utils/dbConnect'
 import Product from '../../../models/Product'
 
 export default async function productsHandler(req, res) {
-  // await dbConnect()
+  await dbConnect()
 
-  // const products = await Product.find({})
+  const products = await Product.find({})
 
-  // res.status(200).json(products)
-  console.log('Entered the serverless function')
-
-  return { dummy: 'data' }
+  res.status(200).json(products)
 }
