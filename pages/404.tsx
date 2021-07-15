9f8ad2f0345cfb4f3c070ef97b0e7dbb179f1791
jeffhogg86 <jeffhogg86@gmail.com>
2021-07-14 21:00:43 -0700

    add logo

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,14 +1,17 @@
+import Image from 'next/image'
+import { Container } from 'react-bootstrap'
 import Layout from '../components/Layout'
 
 const NotFound = () => {
   return (
     <Layout title='Page Not Found'>
-      <div className='flex flex-col items-center mt-20'>
+      <Container className='text-center'>
+        <Image src='/images/cardshoplogo.png' width={150} height={150} />
         <h1 className='text-6xl my-5'>Sorry!</h1>
         <h2 className='text-4xl text-gray-400 mb-5'>
           This page does not exist
         </h2>
-      </div>
+      </Container>
     </Layout>
   )
 }
